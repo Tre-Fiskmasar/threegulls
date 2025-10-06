fa25c084783c401d17aec4cfd982dfcf13fd42ee
Hampus <hampus.b.karlstrom@gmail.com>
2025-10-06 10:06:05 +0200

    added comments in send-api + changed content of Hampus.json

--- www-root/user/send-api.js
+++ www-root/user/send-api.js
@@ -3,11 +3,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   axios.get("http://localhost:5150/api/seagulls.php", {
     headers: { "X-API-Key": apiKey }
-  })
+  }) //send API key from seagulls.php with axios
     .then(res => {
       
       console.log("Status:", res.status);
-      console.log("Data:", res.data);
+      console.log("Data:", res.data); 
+      //print the status + data of get request 
     })
     .catch(err => console.error("Error:", err));
+    //if theres an error, catch it and print it to console
 });
